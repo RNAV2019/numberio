1e519ab8fc5d96b754d4ef8917a80658ea9d2a5d
RNAV2022 <ryannav2019@gmail.com>
2022-12-09 16:57:13 +0000

    Initial Commit

---
+++ numberio/numberioApp.swift
@@ -0,0 +1,17 @@
+//
+//  numberioApp.swift
+//  numberio
+//
+//  Created by Ryan Navsaria on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct numberioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
